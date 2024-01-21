b872c0b850f63583b51a4fe6e56a3bc556864c39
Dmitry Petsevich <dmitry.petsevich@gmail.com>
2024-01-21 15:11:58 +0300

    [master] Изменил рендер компонента App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.style.css';
+import PageContent from './components/page-content/PageContent';
+import NavigationPanel from './components/navigation-panel/NavigationPanel';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <NavigationPanel />
+      <PageContent />
+    </div>
+  );
 }
 
 export default App;
