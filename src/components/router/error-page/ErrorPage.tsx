630ac20c7f3e62ebf1586d2ca2458be066f8a5f4
Dmitry Petsevich <dmitry.petsevich@gmail.com>
2024-01-21 14:44:34 +0300

    [master] Добавил компонент ErrorPage.

---
+++ src/components/router/error-page/ErrorPage.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { useRouteError } from 'react-router-dom';
+import './ErrorPage.style.css';
+
+const ErrorPage: FC = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>
+          {(error as { statusText: string }).statusText ||
+            (error as Error).message}
+        </i>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
