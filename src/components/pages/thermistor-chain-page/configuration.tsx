e5fb0e34eae88df50694e8aae0943d5bee574f79
Dmitry Petsevich <dmitry.petsevich@gmail.com>
2024-01-22 22:08:45 +0300

    [master] Добавил логику конфигурации таблицы для термокосы.

---
+++ src/components/pages/thermistor-chain-page/configuration.tsx
@@ -0,0 +1,60 @@
+import { IThermistorChain } from '../../../interfaces/thermistorChain';
+import { IHeader } from '../../table/interfaces';
+
+function configuration(data: IThermistorChain[]) {
+  const uniqueDataColumnKeys = data.reduce((acc, current) => {
+    Object.keys(current.data).forEach((key) => acc.add(key));
+
+    return acc;
+  }, new Set<string>([]));
+
+  const dataColumns: IHeader[] = Array.from(uniqueDataColumnKeys).map(
+    (key) => ({
+      key,
+      label: key,
+      renderCell: (data: IThermistorChain) => {
+        return <span>{data.data[+key]?.value.toFixed(1) || ''}</span>;
+      },
+    })
+  );
+
+  const config: IHeader[] = [
+    {
+      key: 'dateTime',
+      label: 'Дата и время измерения',
+      props: {
+        rowSpan: '2',
+      },
+      renderCell: (data: IThermistorChain) => (
+        <span>{new Date(data.time).toDateString()}</span>
+      ),
+    },
+    {
+      key: 'tAverage',
+      label: 'Средняя температура',
+      renderHead: (
+        <span>
+          T<sub>e</sub>
+        </span>
+      ),
+      props: {
+        rowSpan: '2',
+      },
+      renderCell: (data: IThermistorChain) => (
+        <span>{data.averageTemperature.toFixed(2)}</span>
+      ),
+    },
+    {
+      key: 'deepth',
+      label: 'Глубина, м',
+      children: dataColumns.sort((a, b) => (+a.key > +b.key ? 1 : -1)),
+      props: {
+        colSpan: `${dataColumns.length}`,
+      },
+    },
+  ];
+
+  return config;
+}
+
+export default configuration;
