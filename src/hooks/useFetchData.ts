4c93596ba89eb990a0aa7edb8a0dfb53fce21c10
Dmitry Petsevich <dmitry.petsevich@gmail.com>
2024-01-22 11:14:53 +0300

    [master] Добавил хук useFetchData.

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import useLoaderContext from '../contexts/loader-context/useLoaderContext';
+
+export function useFetchData<T>(url: RequestInfo | URL, options?: RequestInit) {
+  const [data, setData] = useState<T>();
+  const [error, setError] = useState(null);
+  const { setLoading } = useLoaderContext();
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(url, { ...options })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log('error: ', error);
+        setError(error);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+      });
+  }, [url, options, setLoading]);
+
+  return { data, error };
+}
